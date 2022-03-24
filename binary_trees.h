35c78aee5a839ca8cef9744e339f8399b3ed1943
root <alina.delossantos@gmail.com>
2022-03-23 18:53:43 -0700

    updated header

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,7 @@
 #ifndef __BINARY_TREES_H__
 #define __BINARY_TREES_H__
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -26,6 +27,7 @@ typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
